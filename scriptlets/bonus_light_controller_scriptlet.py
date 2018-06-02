32d6fed49e96768b0cc371a8db2c816532ec173b
oliviarium <oliviarium@gmail.com>
2018-06-02 12:33:26 -0700

    511 bonus display cap

--- scriptlets/bonus_light_controller_scriptlet.py
+++ scriptlets/bonus_light_controller_scriptlet.py
@@ -8,7 +8,10 @@ class BonusLightController(Scriptlet):
         self.machine.events.add_handler('player_bonuscount', self._handle_bonus_change)
 
     def _handle_bonus_change(self, **kwargs):
-        binary = '{0:09b}'.format(kwargs.get("value"))
+        intvalue = kwargs.get("value")
+        if intvalue > 511:
+            intvalue = 511
+        binary = '{0:09b}'.format(intvalue)
         for x in range(9):
             event_name = 'bonus_light_{light}_{lit}'.format(light=str(x), lit='on' if binary[8-x] == '1' else 'off')
             self.machine.events.post(event_name)
