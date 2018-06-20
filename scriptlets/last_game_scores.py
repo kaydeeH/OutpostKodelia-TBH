2915d409d611e8cb9e47be40bccca4b23a0de023
Kayden Helm <kaydenhelm@gmail.com>
2018-06-19 23:42:51 -0700

    Added last game slide to attract loop and scriptlet to provide the events and the data.

---
+++ scriptlets/last_game_scores.py
@@ -0,0 +1,25 @@
+from mpf.core.scriptlet import Scriptlet
+
+
+class LastGameScores(Scriptlet):
+
+    def on_load(self):
+        self.debug_log("Flip delay scriptlet loaded!")
+
+        self.machine.events.add_handler('slide_lastgame_created', self._generate_events, 10000)
+
+    def _generate_events(self, **kwargs):
+        del kwargs
+
+        player2 = self.machine.get_machine_var("player2_score")
+        player3 = self.machine.get_machine_var("player3_score")
+        player4 = self.machine.get_machine_var("player4_score")
+
+        if player2:
+            self.machine.events.post('lastgame_player2', score=player2)
+
+        if player3:
+            self.machine.events.post('lastgame_player3', score=player3)
+
+        if player4:
+            self.machine.events.post('lastgame_player4', score=player4)
