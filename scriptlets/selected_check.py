213380004251fdaaa237260a4a7182ea4d304c26
oliviarium <oliviarium@gmail.com>
2018-10-28 13:52:49 -0700

    various updates

--- scriptlets/selected_check.py
+++ scriptlets/selected_check.py
@@ -34,8 +34,23 @@ class SelectedCheck(Scriptlet):
             6: "status_untitled_mode_6",
             7: "status_untitled_mode_7"
         }
+        startedmodes = {
+            1: "status_mode_01_started",
+            2: "status_mode_02_started",
+            3: "status_mode_03_started",
+            4: "status_mode_04_started",
+            5: "status_mode_05_started",
+            6: "status_mode_06_started",
+            7: "status_mode_07_started"
+        }
         player = self.machine.game.player
-        if player.vars.get(modes.get(1)) != 0 and player.vars.get(modes.get(2)) != 0 and player.vars.get(modes.get(3)) != 0 and player.vars.get(modes.get(4)) != 0 and player.vars.get(modes.get(5)) != 0 and player.vars.get(modes.get(6)) != 0:
+
+
+        self.machine.events.post('oh_testing_select_one', value=player.vars.get(startedmodes.get(1)))
+        self.machine.events.post('oh_testing_select_one_b', value=player.vars.get(modes.get(1)))
+
+#        if player.vars.get(modes.get(1)) != 0 and player.vars.get(modes.get(2)) != 0 and player.vars.get(modes.get(3)) != 0 and player.vars.get(modes.get(4)) != 0 and player.vars.get(modes.get(5)) != 0 and player.vars.get(modes.get(6)) != 0:
+        if player.vars.get(startedmodes.get(1)) != 0 and player.vars.get(startedmodes.get(2)) != 0 and player.vars.get(startedmodes.get(3)) != 0 and player.vars.get(startedmodes.get(4)) != 0 and player.vars.get(startedmodes.get(5)) != 0 and player.vars.get(startedmodes.get(6)) != 0:
             self.machine.events.post('selector2_mode7_selected')
         else:
             for ourMode in modes:
--- scriptlets/text_type_in.py
+++ scriptlets/text_type_in.py
@@ -19,20 +19,51 @@ class TextTypeIn(Scriptlet):
     def on_load(self):
         self.debug_log("text typer started!")
         self.machine.events.add_handler('set_text_to_cycle', self._set_text)
+        self.machine.events.add_handler('slide_video_removed', self._clear_mach)
+
+    def _clear_mach(self, **kwargs):
+        self.machine.set_machine_var(name="thePinExp", value="")
+        self.machine.set_machine_var(name="s1", value="")
+        self.machine.set_machine_var(name="s2", value="")
+        self.machine.set_machine_var(name="s3", value="")
+        self.machine.set_machine_var(name="s4", value="")
+        self.machine.set_machine_var(name="s5", value="")
+        self.machine.set_machine_var(name="s6", value="")
 
     def _set_text(self, **kwargs):
         TextTypeIn.textValue = kwargs.get("text")
+
+        if TextTypeIn.textValue == "s6":
+            #TextTypeIn.textValue = str(self.machine.get_machine_var("score6_label")) + "-" + str(self.machine.get_machine_var("score6_name")) + " " + str(self.machine.get_machine_var("score6_value"))
+            TextTypeIn.textValue = str(self.machine.get_machine_var("score6_label")) + "-" + str(self.machine.get_machine_var("score6_name")) + " " + "{:,}".format(self.machine.get_machine_var("score6_value"))
+
+        if TextTypeIn.textValue == "s5":
+            TextTypeIn.textValue = str(self.machine.get_machine_var("score5_label")) + "-" + str(self.machine.get_machine_var("score5_name")) + " " + "{:,}".format(self.machine.get_machine_var("score5_value"))
+
+        if TextTypeIn.textValue == "s4":
+            TextTypeIn.textValue = str(self.machine.get_machine_var("score4_label")) + "-" + str(self.machine.get_machine_var("score4_name")) + " " + "{:,}".format(self.machine.get_machine_var("score4_value"))
+
+        if TextTypeIn.textValue == "s3":
+            TextTypeIn.textValue = str(self.machine.get_machine_var("score3_label")) + "-" + str(self.machine.get_machine_var("score3_name")) + " " + "{:,}".format(self.machine.get_machine_var("score3_value"))
+
+        if TextTypeIn.textValue == "s2":
+            TextTypeIn.textValue = str(self.machine.get_machine_var("score2_label")) + "-" + str(self.machine.get_machine_var("score2_name")) + " " + "{:,}".format(self.machine.get_machine_var("score2_value"))
+
+        if TextTypeIn.textValue == "s1":
+            TextTypeIn.textValue = str(self.machine.get_machine_var("score1_label")) + "-" + str(self.machine.get_machine_var("score1_name")) + " " + "{:,}".format(self.machine.get_machine_var("score1_value"))
+
         TextTypeIn.delayMs = kwargs.get("delay")
         TextTypeIn.varName = kwargs.get("varname")
         TextTypeIn.intermediate = kwargs.get("intermed")
         TextTypeIn.textDisplay = TextTypeIn.intermediate
-        self.machine.game.player[TextTypeIn.varName] = TextTypeIn.textDisplay
+        #self.machine.machine_vars[TextTypeIn.varName] = TextTypeIn.textDisplay
+        self.machine.set_machine_var(name=TextTypeIn.varName, value=TextTypeIn.textDisplay)
         DelayManager(self.machine.delayRegistry).add(callback=self._text_timer, ms=TextTypeIn.delayMs)
 
     def _text_timer(self, **kwargs):
         del kwargs
 
-        self.machine.events.post('debug_timer', val=TextTypeIn.textValue, tex=TextTypeIn.textDisplay)
+#        self.machine.events.post('debug_timer', val=TextTypeIn.textValue, tex=TextTypeIn.textDisplay)
 
         if TextTypeIn.textValue != TextTypeIn.textDisplay:
            strlen = len(TextTypeIn.textDisplay)
@@ -47,6 +78,7 @@ class TextTypeIn(Scriptlet):
                TextTypeIn.textDisplay += TextTypeIn.intermediate
 
            # self.machine.game.player.typed_display_text = TextTypeIn.textDisplay
-           self.machine.game.player[TextTypeIn.varName] = TextTypeIn.textDisplay
+#          # self.machine.machine_vars[TextTypeIn.varName] = TextTypeIn.textDisplay
+           self.machine.set_machine_var(name=TextTypeIn.varName, value=TextTypeIn.textDisplay)
            DelayManager(self.machine.delayRegistry).add(callback=self._text_timer, ms=TextTypeIn.delayMs)
 
