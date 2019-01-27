a611444ba8e6222f5c07129cfb12cee419d6c78e
oliviarium <oliviarium@gmail.com>
2019-01-27 00:04:43 -0700

    Fix --  Bonus crash on Tilt if bonus is built up  Failure to eject scoop on certain modes  Eliminate possibility to double collect on david v golliath  Disable ball search -- doesn't work well with scoop

--- scriptlets/bonus_countdown_scriptlet.py
+++ scriptlets/bonus_countdown_scriptlet.py
@@ -1,14 +1,14 @@
 from mpf.core.scriptlet import Scriptlet
 from mpf.core.delays import DelayManager
 
-delay_time = None  # type: int
+delay = None  # type: int
 
 
 class BonusCountdown(Scriptlet):
     def on_load(self):
         self.debug_log("Bonus Countdown scriptlet loaded!")
         self.machine.events.add_handler('bonus_product', self._start_bonus_countdown)
-        self.machine.events.add_handler('player_bonuscount', self._decrement_bonus, 1)
+        self.machine.events.add_handler('bonus_countdown_decrement', self._decrement_bonus, 1)
 
     def _start_bonus_countdown(self, **kwargs):
         del kwargs
@@ -18,16 +18,16 @@ class BonusCountdown(Scriptlet):
                 BonusCountdown.delay = 100
             self.machine.game.player.bonuscount -= 1
             self.machine.game.player.bonuscountdisplay += 1
+            self.machine.events.post('bonus_countdown_decrement')
 
     def _decrement_bonus(self, **kwargs):
-        intchange = kwargs.get("change")
-        if intchange < 0:
             DelayManager(self.machine.delayRegistry).add(callback=self._do_decrement, ms=BonusCountdown.delay)
 
     def _do_decrement(self):
         if self.machine.game.player.bonuscount > 0:
             self.machine.game.player.bonuscount -= 1
             self.machine.game.player.bonuscountdisplay += 1
+            self.machine.events.post('bonus_countdown_decrement')
         else:
             self.machine.events.post('bonus_subtotal_display',
                                      score=self.machine.game.player.bonuscountdisplay * self.machine.game.player.pds)
